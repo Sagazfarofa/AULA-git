6eb29aca8ea116efcffa61d3b87f29ed9f7c5f9d
sagazfarofa <luiz.g.rosa9@aluno.senai.br>
2025-02-25 12:59:54 -0300

    teste

--- index.js
+++ index.js
@@ -1,2 +1,7 @@
 console.log("ola git!");
 console.log("primeiro Branch");
+console.log("nova mensagem de alteração");
+console.log("criando alteração no novo branch!");
+
+console.log("projeto adiantado");
+console.log("segundo merge");
